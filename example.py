fb7ca91d11f0aabce20f2247c78fea23429f6323
rigrassm <rigrassm@gmail.com>
2018-09-09 21:09:44 -0500

    Initial Commit

---
+++ example.py
@@ -0,0 +1,28 @@
+from sys import argv
+from pathlib import Path
+from pprint import pprint as pp
+from parse_inventory import Inventory
+
+
+def main():
+    try:
+        inv_csv_file = Path(argv[1])
+        inventory = Inventory(inv_csv_file)
+
+        pp(inventory.items)
+        pp(inventory.certs)
+        pp(inventory.item_types)
+
+        pp(inventory.get_items_by_paint("Titanium White"))
+        pp(inventory.get_items_by_cert('shotsongoal'))
+        pp(inventory.get_items_by_type('player banner'))
+    except FileNotFoundError:
+        print(f'Error: File not found\n\t{inv_csv_file}')
+        exit(1)
+    except IndexError:
+        print("Error: Must provide path to inventory CSV file")
+        exit(1)
+
+
+if __name__ ==  '__main__':
+    main()
---
+++ parse_inventory.py
@@ -0,0 +1,87 @@
+#!/bin/env python3
+import csv
+from os import path
+
+
+class Inventory:
+
+    def __init__(self, inv_file):
+        if path.isfile(inv_file):
+            self.inv_file = inv_file
+        else:
+            raise FileNotFoundError
+
+        self.items, self.item_types, self.certs = self.load_inventory()
+
+
+    def load_inventory(self):
+        items = dict()
+        types = list()
+        certs = list()
+
+        with open(self.inv_file, 'r') as inv:
+            reader = csv.DictReader(inv)
+            for row in reader:
+
+                if row['slot'].lower() not in types:
+                    types.append(row['slot'].lower())
+
+                if row['certification'].lower() not in certs:
+                    certs.append(row['certification'].lower())
+
+                if row['name'] not in items:
+                    items[row['name']] = dict(item_type=row['slot'].lower(), id=row['product id'], inv=list())
+
+                if row['certification'] == 'none':
+                    certified = False
+                    cert = None
+                else:
+                    certified = True
+                    cert = row['certification']
+
+                if row['paint'] == 'none':
+                    painted = False
+                    paint = None
+                else:
+                    painted = True
+                    paint = row['paint'].lower()
+
+                if row['tradeable'] == 'false':
+                    tradeable = False
+                else:
+                    tradeable = True
+
+                items[row['name']]['inv'].append(dict(painted=painted,
+                                                      paint=paint,
+                                                      certified=certified,
+                                                      certification=cert,
+                                                      quality=row['quality'],
+                                                      tradeable=tradeable
+                                          ))
+            return items, types, certs
+
+
+    def get_items_by_type(self, item_type):
+        return {k:v for (k,v) in self.items.items() if v['item_type'] == item_type.lower()}
+
+
+    def get_items_by_paint(self, item_color):
+        matches = list()
+        for k,v in self.items.items():
+            for i in v['inv']:
+                if i['painted']:
+                    if i['paint'] == item_color.lower():
+                        i['name'] = k
+                        matches.append(i)
+        return matches
+
+
+    def get_items_by_cert(self, item_cert):
+        matches = list()
+        for k,v in self.items.items():
+            for i in v['inv']:
+                if i['certified']:
+                    if i['certification'].lower() == item_cert.lower().replace(" ",""):
+                        i['name'] = k
+                        matches.append(i)
+        return matches
